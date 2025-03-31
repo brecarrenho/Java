2a99c7f8563a04d83db9a0b5074898c42fad0a95
brecarrenho <brenda.carrenho1a@gmail.com>
2025-03-31 14:01:13 -0300

    Exercício

---
+++ Ex_14.java
@@ -0,0 +1,14 @@
+/**/
+
+/**/
+
+
+
+public class Ex_14 {
+    public static void main(String[] args) {
+        // Laço para exibir a mensagem 10 vezes
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Eu gosto de estudar Algoritmos!");
+        }
+    }
+}
